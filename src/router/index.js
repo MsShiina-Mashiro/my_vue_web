71be4c9b4aac5f6f5559a45b802f3f4c4a788c46
Jie.Huang <929661205@qq.com>
2020-05-17 23:48:04 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,29 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/',
+        component: () => import('../views/Hello.vue'),
+        meta: 'about'
+      },
+      {
+        path: '/about',
+        component: () => import('../views/About.vue'),
+        meta: 'photo'
+      },
+      {
+        path: '/photo',
+        component: () => import('../views/Photo.vue'),
+        meta: 'connect'
+      },
+      {
+        path: '/connect',
+        component: () => import('../views/Connect.vue')
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header></Header>
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Header: () => import('../components/Header.vue')
   }
 }
 </script>
+
+<style lang="less" scoped>
+ 
+</style>
